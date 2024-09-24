8509530c0cf195e2b86dfe86233c965fa99a4109
duytrung <duytrung341@gmail.com>
2024-09-24 15:25:17 +0700

    chore: update opengraph-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,8 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import ThemeSwitcher from './(home)/_components/theme-switcher';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Next Tiptap',
-  description:
-    'A modern WYSIWYG rich text editor based on tiptap and shadcn ui for ReactJs/NextJs',
-  keywords: 'Tiptap, WYSIWYG, Rich Text Editor, ReactJS, NextJS',
-  metadataBase: new URL(`https://next-tiptap.vercel.app`),
-  openGraph: {
-    type: 'website',
-    url: `https://next-tiptap.vercel.app`,
-    title: 'Next Tiptap',
-    description:
-      'A modern WYSIWYG rich text editor based on tiptap and shadcn ui for ReactJs/NextJs',
-    siteName: 'Next Tiptap',
-    locale: 'en_US'
-  }
-};
 
 export default function RootLayout({
   children
